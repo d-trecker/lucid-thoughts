6d157e125fff4b79b311b87a716d6f3699dbba59
Dylan <79778111+d-trecker@users.noreply.github.com>
2021-07-25 04:29:36 -0700

    Set up models

---
+++ models/Thought.js
@@ -0,0 +1,52 @@
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const ReactionSchema = new Schema(
+    {
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+      },
+      reactionBody: {
+        type: String,
+        required: true, 
+        trim: true
+      },
+      username: {
+        type: String,
+        required: true
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+      }
+    },
+    {
+      toJSON: {
+        getters: true
+      }
+    }
+  );
+
+const ThoughtSchema = new Schema ({
+    thoughtText: {
+        type: String,
+        required: true, 
+        //Length between 1 -280
+    },
+    createdAt: {
+        type: Date, 
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [ReactionSchema]
+});
+
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,42 @@
+const {Schema, model } = require("mongoose");
+const UserSchema = new Schema ({
+    userName: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        //Match a vailid email address
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+});
+
+UserSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
